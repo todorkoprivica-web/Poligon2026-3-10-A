492bd7286322947bcaddc14b8eddab994e05eaa2
Prva gimnazija <prvabeogim2026@outlook.com>
2026-03-13 17:37:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligon2026_3_10_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
