388ab5e3b41468bea26c885ef4398a8df0cf1a8f
Prva gimnazija <prvabeogim2026@outlook.com>
2026-03-20 12:03:13 +0000

    fda

--- poligon.cs
+++ poligon.cs
@@ -27,8 +27,15 @@ namespace Poligon2026_3_10_A
                 Console.WriteLine("Ay({0})=", i + 1);
                 double y = Convert.ToDouble(Console.ReadLine());
                 teme[i].x = x;teme[i].y = y; 
-
             }
         }
+        public void stampaj() {
+            Console.WriteLine("Poligon od " + br_temena + "tacaka");
+            for (int i = 0; i < br_temena; i++)
+            {
+                Console.WriteLine("A({0})"+"("+teme[i].x+","+teme[i].y+")",i+1);
+            }
+        }
+
     }
 }
